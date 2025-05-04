0463fee9faa8d08e8fcec1486cbe25c569d60aa7
root <bmmaxbb@gmail.com>
2025-05-03 21:29:11 -0700

    Fix stairs db requests

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
 FROM python:3.7-slim
 
-RUN apt-get -y update
-# for dlib
-RUN apt-get install -y build-essential cmake
-# for opencv
-RUN apt-get install -y libopencv-dev
+ENV PYTHONUNBUFFERED=1
 
 # pip install
 RUN pip install paho-mqtt \
   && pip install mysql-connector-python \
-    && pip install typing-extensions
+    && pip install typing-extensions \
+    && pip install python-dotenv
 
 COPY . .
 
--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import mysql.connector
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +11,7 @@ def on_connect(client, userdata, flags, reason_code, properties):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("garage/buttons/b1")
-
+    client.subscribe("house/stairs/sensors/s1")
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
@@ -25,13 +26,14 @@ def on_button_message(client, userdata, msg):
     :return:
     """
 
+    print(msg.topic+" "+str(msg.payload))
+
     if not mydb.is_connected():
         mydb.reconnect()
 
     sql = "INSERT INTO bell () VALUES()"
     res = cursor.execute(sql)
     mydb.commit()
-    print(res)
 
 def on_stairs_message(client, userdata, msg):
     """
@@ -42,14 +44,23 @@ def on_stairs_message(client, userdata, msg):
     :param msg:
     :return:
     """
+    print(msg.topic+" "+str(msg.payload))
+    
+    data = json.loads(msg.payload)
 
-    sql = "INSERT INTO "
+    if not mydb.is_connected():
+        mydb.reconnect()
+
+    sql = ("INSERT INTO motion_stairs (illumination, motion, motion_energy, presence, presence_energy) "
+            "VALUES(%s, %s, %s, %s, %s)")
+    res = cursor.execute(sql, (data.get('illumination'), data.get('movement'), data.get('movement_energy'), data.get('presence', 'None'), data.get('presence_energy')))
+    mydb.commit()
 
 mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
 mqttc.message_callback_add("garage/buttons/b1", on_button_message)
-
+mqttc.message_callback_add("house/stairs/sensors/s1", on_stairs_message)
 
 mqttc.connect("192.168.0.123", 1883, 60)
 
@@ -60,6 +71,8 @@ mydb = mysql.connector.connect(
     password=os.getenv("MYSQL_PASSWORD"),
     database="iot"
 )
+
+print('Connected to MySQL with user ' + os.getenv("MYSQL_USER"))
 
 cursor = mydb.cursor()
 
